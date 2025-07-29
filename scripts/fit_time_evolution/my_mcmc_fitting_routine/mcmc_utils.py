0d7877bb93feaef486518e65eb62167a8d7e15be
neco kriel <necokriel@gmail.com>
2025-07-29 16:55:23 +1000

    move unil funcs to where they are used

--- scripts/fit_time_evolution/my_mcmc_fitting_routine/mcmc_utils.py
+++ scripts/fit_time_evolution/my_mcmc_fitting_routine/mcmc_utils.py
@@ -21,39 +21,5 @@ def plot_param_percentiles(ax, samples, orientation):
   ax_line(p50, color="green", ls=":", lw=1.5, zorder=5)
   ax_span(p16, p84, color="green", ls="-", lw=1.5, alpha=0.3, zorder=4)
 
-def compute_median_params_from_kde(kde, num_samples=10000):
-  samples = kde.resample(num_samples)
-  return tuple(numpy.median(samples, axis=1))
-
-def compute_binned_data(x_values, y_values, num_bins):
-  x_bin_edges = numpy.linspace(0, numpy.max(x_values), num_bins+1)
-  x_bin_centers = 0.5 * (x_bin_edges[1:] + x_bin_edges[:-1])
-  x_bin_indices = numpy.digitize(x_values, x_bin_edges) - 1
-  y_ave_s = numpy.zeros(num_bins)
-  y_std_s = numpy.zeros(num_bins)
-  log10_y_ave_s = numpy.zeros(num_bins)
-  log10_y_std_s = numpy.zeros(num_bins)
-  for bin_index in range(num_bins):
-    bin_mask = (x_bin_indices == bin_index)
-    if numpy.any(bin_mask):
-      y_values_in_bin = numpy.array(y_values)[bin_mask]
-      log10_y_values_in_bin = numpy.log10(y_values_in_bin)
-      y_ave_s[bin_index] = numpy.mean(y_values_in_bin)
-      y_std_s[bin_index] = numpy.std(y_values_in_bin)
-      log10_y_ave_s[bin_index] = numpy.mean(log10_y_values_in_bin)
-      log10_y_std_s[bin_index] = numpy.std(log10_y_values_in_bin)
-    else:
-      y_ave_s[bin_index] = numpy.nan
-      y_std_s[bin_index] = numpy.nan
-      log10_y_ave_s[bin_index] = numpy.nan
-      log10_y_std_s[bin_index] = numpy.nan
-  return {
-    "x_bin_centers": x_bin_centers,
-    "y_ave_s": y_ave_s,
-    "y_std_s": y_std_s,
-    "log10_y_ave_s": log10_y_ave_s,
-    "log10_y_std_s": log10_y_std_s
-  }
-
 
 ## END OF MODULE
